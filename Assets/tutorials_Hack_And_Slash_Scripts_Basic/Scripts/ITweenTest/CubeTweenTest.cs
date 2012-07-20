644cce8282400f460b2c48149ea0e1fa2fd5a10b
senkay <senkay.lee@gmail.com>
2012-07-20 20:37:29 +0800

    提交

--- Assets/tutorials_Hack_And_Slash_Scripts_Basic/Scripts/ITweenTest/CubeTweenTest.cs
+++ Assets/tutorials_Hack_And_Slash_Scripts_Basic/Scripts/ITweenTest/CubeTweenTest.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CubeTweenTest : MonoBehaviour {
 
@@ -8,7 +9,14 @@ public class CubeTweenTest : MonoBehaviour {
 		//移动
 		//iTween.MoveTo(gameObject, new Vector3(20,gameObject.transform.position.y, gameObject.transform.position.z), 20);
 		//振动
-		iTween.ShakePosition(gameObject, new Vector3(0,(0.1),0), 1);
+		//iTween.ShakePosition(gameObject, new Vector3(0,(0.1),0), 1);
+		Vector3[]  paths = iTweenPath.GetPath("myPath");
+		Debug.Log("paths="+paths);
+		
+		iTween.MoveTo(gameObject, iTween.Hash("positon",Vector3(0,0,0),
+			"path",paths,
+			"time", 20,
+			"easetype","linear"));
 	}
 	
 	// Update is called once per frame
